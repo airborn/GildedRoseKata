457752a53e71b60941e981c24caa15214460697d
Jacek Jackowiak <jacek1986@gmail.com>
2015-05-16 22:42:30 +0200

    Inverting if's conditions

--- src/main/java/pl/airborn/GildedRose.java
+++ src/main/java/pl/airborn/GildedRose.java
@@ -12,13 +12,7 @@ public class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.getName().equals("Aged Brie") && !item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.getQuality() > 0) {
-                    if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
-                        item.setQuality(item.getQuality() - 1);
-                    }
-                }
-            } else {
+            if (item.getName().equals("Aged Brie") || item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (item.getQuality() < 50) {
                     item.setQuality(item.getQuality() + 1);
 
@@ -36,6 +30,12 @@ public class GildedRose {
                         }
                     }
                 }
+            } else {
+                if (item.getQuality() > 0) {
+                    if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
+                        item.setQuality(item.getQuality() - 1);
+                    }
+                }
             }
 
             if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
@@ -43,19 +43,19 @@ public class GildedRose {
             }
 
             if (item.getSellIn() < 0) {
-                if (!item.getName().equals("Aged Brie")) {
-                    if (!item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.getName().equals("Aged Brie")) {
+                    if (item.getQuality() < 50) {
+                        item.setQuality(item.getQuality() + 1);
+                    }
+                } else {
+                    if (item.getName().equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        item.setQuality(0);
+                    } else {
                         if (item.getQuality() > 0) {
                             if (!item.getName().equals("Sulfuras, Hand of Ragnaros")) {
                                 item.setQuality(item.getQuality() - 1);
                             }
                         }
-                    } else {
-                        item.setQuality(0);
-                    }
-                } else {
-                    if (item.getQuality() < 50) {
-                        item.setQuality(item.getQuality() + 1);
                     }
                 }
             }
